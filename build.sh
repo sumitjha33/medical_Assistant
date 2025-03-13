b240b358fc71f4a0824e833c49dd697b73a87e98
sumitjha33 <sumitjha3333@gmail.com>
2025-03-13 10:03:41 +0530

    Add build script and Git attributes for line ending handling

--- build.sh
+++ build.sh
@@ -8,12 +8,20 @@ pip install --no-cache-dir -r requirements.txt
 export TRANSFORMER_CACHE="/opt/render/project/src/.cache/huggingface"
 mkdir -p $TRANSFORMER_CACHE
 
-# Pre-download models
-python -c "
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L3-v2', cache_folder='$TRANSFORMER_CACHE')
-"
-
 # Set environment variables
 export SENTENCE_TRANSFORMERS_HOME=$TRANSFORMER_CACHE
 export TRANSFORMERS_CACHE=$TRANSFORMER_CACHE
+export PYTORCH_NO_CUDA_MEMORY_CACHING=1
+export TRANSFORMERS_OFFLINE=1
+
+# Clean any existing cache
+rm -rf $TRANSFORMER_CACHE/*
+
+# Pre-download only the smaller model
+python -c "
+from sentence_transformers import SentenceTransformer
+import gc
+model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L3-v2', cache_folder='$TRANSFORMER_CACHE')
+del model
+gc.collect()
+"
